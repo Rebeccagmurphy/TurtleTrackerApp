da79969ffbe02bac087f3db0729204e0372e0331
Rebecca Murphy <rgm37@duke.edu>
2021-09-23 15:38:50 -0400

    Iterates through all records

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -22,18 +22,20 @@ line_list = file_object.readlines()
 #Close the file
 file_object.close()
 
-lineString = line_list[100]
+#Iterate through all lines in the lineList
+for lineString in line_list[17:]:
+    if lineString[0] in ('#', 'u'): continue
 # =============================================================================
 
-#Split the string into a list of data items
-lineData = lineString.split()
-
-#Extract items in list into variables
-record_id = lineData[0]
-obs_date = lineData[2]
-obs_lc = lineData[4]
-obs_lat = lineData[6]
-obs_long = lineData[7]
-
-#Print the location of Sarah
-print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, long:{obs_long}.")
+    #Split the string into a list of data items
+    lineData = lineString.split()
+    
+    #Extract items in list into variables
+    record_id = lineData[0]
+    obs_date = lineData[2]
+    obs_lc = lineData[4]
+    obs_lat = lineData[6]
+    obs_long = lineData[7]
+    
+    #Print the location of Sarah
+    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, long:{obs_long}.")
