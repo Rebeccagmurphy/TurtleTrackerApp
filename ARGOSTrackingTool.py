475b6baccb582ba8960bfbac04a0b2f82b35fac5
Rebeccagmurphy <89416821+Rebeccagmurphy@users.noreply.github.com>
2021-09-26 10:46:02 -0400

    Filters out lc not in 1,2,3

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -22,6 +22,10 @@ line_list = file_object.readlines()
 #Close the file
 file_object.close()
 
+#Create two empty dictionary objects
+date_dict = {}
+coord_dict = {}
+
 #Iterate through all lines in the lineList
 for lineString in line_list[17:]:
     if lineString[0] in ('#', 'u'): continue
@@ -34,8 +38,14 @@ for lineString in line_list[17:]:
     record_id = lineData[0]
     obs_date = lineData[2]
     obs_lc = lineData[4]
+  
+   #if obs_lc not in ("1", "2", "3"):
+       # continue
     obs_lat = lineData[6]
     obs_long = lineData[7]
     
-    #Print the location of Sarah
-    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, long:{obs_long}.")
+    #Print the location of Sarah if lc is 1, 2, or 3
+    if obs_lc in ("1", "2", "3"):
+        print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, long:{obs_long}.")
+        date_dict[record_id] = obs_date
+        coord_dict[record_id] = (obs_lat,obs_long)
